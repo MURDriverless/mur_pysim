d9430aa0c38917e31312608ccf15d5739c79ccd9
Joseph Leong <leong.joseph@me.com>
2020-04-22 00:11:48 +1000

    added functionality to checkpoints.py

--- utils/checkpoints.py
+++ utils/checkpoints.py
@@ -7,17 +7,17 @@ import numpy as np
 
 
 class Checkpoint:
-    current_index = 0
+    index = 0
 
     def __init__(self, track_coordinates: dict):
         self.track_coordinates = track_coordinates
 
-    def check(self, car_pos: (int, int), threshold=5) -> int:
+    def check_dist(self, car_pos: (int, int), threshold=5) -> int:
         """
             Calculates the Euclidean distance from the car's current position to the next checkpoint
             If this distance is less than the threshold, the new checkpoint will is designated.
         """
-        obj_pos = self.track_coordinates.get(self.current_index)
+        obj_pos = self.track_coordinates.get(self.index)
 
         x = np.array([car_pos[0], obj_pos[0]])
         y = np.array([car_pos[1], obj_pos[1]])
@@ -26,13 +26,29 @@ class Checkpoint:
         y_dist = np.sqrt((y[0] - y[1]) ** 2)
 
         dist = x_dist + y_dist
-        print(dist)
 
         if dist < threshold:
-            self.current_index += 1
+            self.index += 1
 
         return dist
 
+    def check_xytheta_dist(self, x_pos, y_pos):
+        """
+            Calculates the difference between the x_pos and y_pos of the car and the x_pos and
+            y_pos of the objective. Additionally, calculates the angle between obj and car.
+
+        :return: x_diff (np.float32), y_diff (np.float32), theta (np.float32)
+        """
+        obj_pos = self.track_coordinates.get(self.index)
+
+        x_delta = np.float32(x_pos - obj_pos[0])
+        y_delta = np.float32(y_pos - obj_pos[1])
+        theta = np.arctan(x_diff / y_diff)
+
+        return x_delta, y_delta, theta
+
+
     @classmethod
     def reset(cls):
-        cls.current_index = 0
+        cls.index = 0
+
