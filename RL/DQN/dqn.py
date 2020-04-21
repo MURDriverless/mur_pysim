a1d58ae50d069f92b454f2062b3a6fdfa361e0c1
Joseph Leong <leong.joseph@me.com>
2020-04-22 00:27:10 +1000

    deep Q network structure added

--- RL/DQN/dqn.py
+++ RL/DQN/dqn.py
@@ -1,5 +1,33 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import numpy as np
+
+
+class DeepQNetwork(nn.Module):
+    """
+
+    """
+    def __init__(self, lr, input_dims, l1_dims, l2_dims, output_dims):
+        super(DeepQNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.l1_dims = l1_dims
+        self.l2_dims = l2_dims
+        self.output_dims = output_dims
+
+        self.l1 = nn.Linear(*self.input_dims, self.l1_dims)
+        self.l2 = nn.Linear(self.l1_dims, self.l2_dims)
+        self.l3 = nn.Linear(self.l2_dims, output_dims)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.loss = nn.MSELoss()
+        self.device = T.device('cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        actions = self.fc1(x)
+
+        return actions
+
+
