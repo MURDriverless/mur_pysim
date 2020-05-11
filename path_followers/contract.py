1748f497c188d32384c56bd01cc541fc0263ce09
Dennis Wirya <dwirya@outlook.com>
2020-05-11 11:40:04 +1000

    Add documentation on the reason for using numpy.ndarray

--- path_followers/contract.py
+++ path_followers/contract.py
@@ -8,8 +8,8 @@ class PathFollowerContract(metaclass=ABCMeta):
         Issues actuation commands to motor control.
 
         Args:
-            states (list of float): Current states observed by Perception.
-            reference (list of float, optional): An array of of the same size as states.
+            states (numpy.ndarray): Current states observed by Perception.
+            reference (numpy.ndarray, optional): An array of of the same size as states.
                 Its purpose is to correct the current state using the reference states,
                 so `reference` needs to share the same structure as `states`. This is
                 typically passed on from Path Planner. If there is a None entry, the
@@ -17,16 +17,20 @@ class PathFollowerContract(metaclass=ABCMeta):
                 is supposedly 0 error.
 
         Returns:
-            list of float: Actuation commands to be sent to EMC. Has the following format:
-                (throttle, steering angle rate)
+            numpy.ndarray: Actuation commands to be sent to EMC (input to the motor plant)
+                Has the format:(throttle, steering angle rate)
                 - throttle: constrained between -1 and 1. 1 signifies maximum acceleration,
                   while -1 indicates maximum deceleration.
                 - steering angle rate: unsure of the constraints for now, will update later.
 
         Notes:
-            `states` will contain an array of cone positions, and in this particular case,
+            1. `states` will contain an array of cone positions, and in this particular case,
             the `reference` should be set to None, and developers are encouraged to explain
             why they are only accessing the first n elements of the reference, because
             the last one is related to the cones.
+            2. We are using numpy.ndarray as the standard format for array instead of Python's
+            in-built list. This is because the size of ndarray is static after creation, so it
+            is supposedly faster at read and write processes. Python's list on the other hand,
+            allows for dynamic growing, so it is less favoured
         """
         pass
--- path_planners/contract.py
+++ path_planners/contract.py
@@ -8,14 +8,17 @@ class PathPlannerContract(metaclass=ABCMeta):
         Generates reference signals (not limited to positions)
 
         Args:
-            states (list of float): Current states observed by Perception.
+            states (numpy.ndarray): Current states observed by Perception.
 
         Returns:
-            list of float: An array of the same size as states. Path Planners
+            numpy.ndarray: An array of the same size as states. Path Planners
                 are intended to generate reference values for the Path Followers.
                 Note that a Path Planner is not required to provide reference
                 states for all of the predictive model states. For instance,
                 an RRT Planner may only provide reference for position, but not
                 velocity. In that case, set the velocity reference entry "None".
+
+        Notes:
+            1. For justification on using numpy.ndarray, see PathFollowerContract
         """
         pass
