7a774a24a1d73c455ea5c4e82b80d300416c3f6b
Dennis Wirya <dwirya@outlook.com>
2020-05-15 22:36:13 +1000

    Handle array reading overflow when obtaining list of cones

--- perception/localisation.py
+++ perception/localisation.py
@@ -17,15 +17,12 @@ def find_nearest_cone(car_position, cone_positions, current_cone_index, searchab
             - next_cone_index (int): index of the nearest next cone from the car
             - next_cone_distance (float): distance from the current car position to the next nearest cone
     """
-    # Only search within the position_index up to n_index_search
-    possible_cones = cone_positions[current_cone_index:(current_cone_index + searchable_size)]
-    dx = np.zeros(len(possible_cones))
-    dy = np.zeros(len(possible_cones))
+    # Only search within the position_index up to searchable_size
+    searchable_cones = cyclic_fetch_elements_in_array(cone_positions, current_cone_index, searchable_size)
 
     # Calculate difference in x and y to compute distance from one cone to next
-    for i in range(len(possible_cones)):
-        dx[i] = car_position[0] - possible_cones[i][0]
-        dy[i] = car_position[1] - possible_cones[i][1]
+    dx = [car_position[0] - cone[0] for cone in searchable_cones]
+    dy = [car_position[1] - cone[1] for cone in searchable_cones]
 
     # Build a list of distances and find the minimum squared distance
     squared_distances = [idx ** 2 + idy ** 2 for (idx, idy) in zip(dx, dy)]
@@ -33,6 +30,37 @@ def find_nearest_cone(car_position, cone_positions, current_cone_index, searchab
 
     # Since we have built a list of distances, we can just get the next cone index by searching for
     # the index of the minimum squared distance value.
-    next_cone_index = squared_distances.index(min_squared_dist) + current_cone_index
+    unadjusted_index = squared_distances.index(min_squared_dist) + current_cone_index
+    # To account for overflown elements in our searchable_cones, we adjust the index by using
+    # the modulo operator "%" over the total number of cones
+    next_cone_index = unadjusted_index % len(cone_positions)
     next_cone_distance = math.sqrt(min_squared_dist)
     return next_cone_index, next_cone_distance
+
+
+def cyclic_fetch_elements_in_array(array, start_index, searchable_size):
+    """
+    Fetches elements without worrying about reaching the end of the array
+
+    Args:
+        array (array_like): anything in the form of array, can be an array of ADT (Abstract Data Type)
+        start_index (int): the starting index to slice from
+        searchable_size (int): the number of elements included from start_index
+
+    Returns:
+        list of object
+    """
+    array_length = len(array)
+    # Determine if we start_index + searchable_size will exceed the array length (overflow),
+    # and if yes, we want to calculate how many elements will exceed, which we call overflow_n.
+    overflow_n = start_index + searchable_size - array_length
+    # If it is larger than 0, that means we have an overflow
+    if overflow_n > 0:
+        # We need to return 2 concatenated arrays:
+        # 1. Elements from the current index to the maximum length of the array
+        # 2. Elements from the start to the overflow_n
+        return array[start_index:array_length] + array[0:overflow_n]
+    else:
+        # Otherwise, if we do not have overflow, return elements from
+        # the starting index up to the searchable size as usual
+        return array[start_index:(start_index + searchable_size)]
